948b0d7fad175135c69f80060dde86c2e4aa361f
brayden-gg <bgoldsteingelb@gmail.com>
2023-04-03 20:50:34 -0400

    fixed some bugs and split into multiple files

---
+++ Assets/ImageProcessing.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; 
+using System.Resources;
+using TMPro;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ImageProcessing 
+{
+
+   public ImageProcessing(){
+
+   }
+   public static Texture2D conv2D(Texture2D image, double[][] filter)
+   {
+      return image;
+   }
+}
