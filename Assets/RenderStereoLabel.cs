9ffaa0e58e2aa8444681e64012a9fbcd250f7b1f
Yuanbo Li <yl4092@columbia.edu>
2023-08-18 20:23:21 -0400

    merged: this version should have both "buggy rotation matrix" and sum_all compute shader working.

--- Assets/RenderStereoLabel.cs
+++ Assets/RenderStereoLabel.cs
@@ -14,14 +14,13 @@ public class RenderStereoLabel : MonoBehaviour
     Quaternion initialRotation;
     Matrix4x4 m;
 
-    // Shader surface_shader;
+    public Shader surface_shader;
     private ComputeBuffer rotation_matrix_buffer;
 
     public ComputeShader cShader;
     private ComputeBuffer sumBuffer;
     private int kernelID_main;
     private int kernelID_init;
-
   
     // Start is called before the first frame update
     void Start()
@@ -41,16 +40,10 @@ public class RenderStereoLabel : MonoBehaviour
         backgroundAndLabelSphereMaterial = backgroundAndLabelSphere.GetComponent<MeshRenderer>().sharedMaterial;
 
         initialRotation = Quaternion.Euler (180f, 270f, 0f); //Should place the label in the middle of the view
-
-        // Matrix4x4 m = Matrix4x4.TRS(Vector3.zero, initialRotation, new Vector3(1,1,1) );
-        // backgroundAndLabelSphereMaterial.SetVector("_LabelRotationMatrixRow1", m.GetRow(0));
-        // backgroundAndLabelSphereMaterial.SetVector("_LabelRotationMatrixRow2", m.GetRow(1));
-        // backgroundAndLabelSphereMaterial.SetVector("_LabelRotationMatrixRow3", m.GetRow(2));
-        // backgroundAndLabelSphereMaterial.SetVector("_LabelRotationMatrixRow4", m.GetRow(3));
-
-        //rotation_matrix
+       
         //this is a 16 float array
         float[] rotation_matrix_array = {1.0F,1.0F,1.0F};
+
         Material material = new Material(surface_shader);
         int stride = System.Runtime.InteropServices.Marshal.SizeOf(typeof(float));
         rotation_matrix_buffer = new ComputeBuffer(16, stride, ComputeBufferType.Default);
@@ -58,14 +51,16 @@ public class RenderStereoLabel : MonoBehaviour
         material.SetBuffer("rotation_matrix", rotation_matrix_buffer);
 
         // Access the screenshot camera
-        ScreenshotCamera = gameObject.GetComponent<Camera>(); 
-        ScreenshotCamera.RenderToCubemap(renderTexture, 63);
+        LabelScreenshotCamera = gameObject.GetComponent<Camera>(); 
+
+        LabelScreenshotCamera.RenderToCubemap(labelRenderTexture, 63);
 
 
         //sum_all
         Debug.Log("SetUp get_sum");
         SetUp_getSum();
         material.SetBuffer ("sum_all_results", sumBuffer);
+
 
 
     }
@@ -80,9 +75,7 @@ public class RenderStereoLabel : MonoBehaviour
 
     //     // // float newXAngle = player.transform.eulerAngles.x;
     //     // // float newYAngle = player.transform.eulerAngles.y;
-    //     //ScreenshotCamera.transform.Rotate(player.transform.rotation[0], player.transform.rotation[1], player.transform.rotation[2]);
-            Update_getSum();
-
+    //     //LabelScreenshotCamera.transform.Rotate(player.transform.rotation[0], player.transform.rotation[1], player.transform.rotation[2]);
     }
 
     // Update is called once per frame
@@ -110,8 +103,8 @@ public class RenderStereoLabel : MonoBehaviour
         kernelID_main = cShader.FindKernel("CSMain");
         kernelID_init = cShader.FindKernel("CSInit");
 
-        cShader.SetTexture(kernelID_main, "InputImage", renderTexture);
-        cShader.SetTexture(kernelID_init, "InputImage", renderTexture);
+        cShader.SetTexture(kernelID_main, "InputImage", labelRenderTexture);
+        cShader.SetTexture(kernelID_init, "InputImage", labelRenderTexture);
 
         cShader.SetBuffer(kernelID_main, "_SumBuffer", sumBuffer);
         cShader.SetBuffer(kernelID_init, "_SumBuffer", sumBuffer);
