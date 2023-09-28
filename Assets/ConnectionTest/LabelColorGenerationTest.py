0a69aee7a9af0d9f4b2a3f59e0751f8697bfa9f3
ashleykwon <hojung_kwon@brown.edu>
2023-09-28 14:30:37 -0400

    Sample optimization code modified

--- Assets/ConnectionTest/LabelColorGenerationTest.py
+++ Assets/ConnectionTest/LabelColorGenerationTest.py
@@ -7,7 +7,7 @@ import numpy as np
 import lpips
 import torchvision
 
-loss_fn = lpips.LPIPS(net='alex',version=0.1)
+loss_fn = lpips.LPIPS(net='vgg',version=0.1) #changed from alex to vgg based on this documentation: https://pypi.org/project/lpips/#b-backpropping-through-the-metric
 loss_fn.cuda()
 
 b_path = "./background.jpg"
@@ -15,22 +15,52 @@ b_w_l_path = "./backgroundAndLabel.jpg"
 backgroundImg = np.asarray(Image.open(b_path))
 backgroundAndLabelImg = np.asarray(Image.open(b_w_l_path))
 
+MAX_ITER = 1000
+costList = []
 
-# custom loss function based on LPIPS distance and difference between background and background+label
-def loss(backgroundAndLabelImg, backgroundImg):
-    # convert the background image and the background+label image to tensors
-    background = lpips.im2tensor(backgroundImg)
-    backgroundAndLabel = lpips.im2tensor(backgroundAndLabelImg)
+
+class Model(torch.nn.Module):
+
+    def __init__(self):
+        super(Model, self).__init__()
+
+    def forward(self, x): # returns the input image, because we're directly modifying the input image
+        return x
+
+# Initialize the model (this is just to connect the input image to the gradient descent function)
+model = Model()
+
+# stochastic gradient descent-based optimizer
+optimizer = torch.optim.SGD([torch.from_numpy(backgroundAndLabelImg)], lr=0.01, momentum=0.9)
+
+# Convert the image with the background only to a tensor
+backgroundImgAsTensor = lpips.im2tensor(backgroundImg)
+
+# Optimize
+for iter in range(MAX_ITER): 
+    if type(backgroundAndLabelImg) == torch.Tensor:
+        backgroundAndLabelImg = backgroundAndLabelImg.numpy()
+
+    # This should just be the output image from the previous iteration
+    backgroundAndLabelImg = model(torch.from_numpy(backgroundAndLabelImg)).numpy()
+
+    # Calculate the LPIPS loss (1 - LPIPS distance between the current backgroundAndLabelImage and the backgroundImage)
+    LPIPSLoss = 1 - loss_fn.forward(lpips.im2tensor(backgroundAndLabelImg).cuda(), backgroundImgAsTensor.cuda())
     
-    # get the LPIPS loss
-    lpipsLoss = -1 * loss_fn.forward(backgroundAndLabel.cuda(), background.cuda())[0][0][0][0].item() 
-    
-    # sum up the lpips and contrast losses
-    return lpipsLoss 
+    # Clear the gradient for a new calculation
+    optimizer.zero_grad()
 
-# Add the optimizer function here and send the resulting array of pixels back to the headset
-loss(backgroundAndLabelImg, backgroundImg)
+    # Do backpropagation based on the LPIPS loss above
+    LPIPSLoss.backward()
 
-# how should we modify the image based on these error terms? 
-# what other error terms should we add?
-# should we use another neural network, along with LPIPS, in the label color generation process?
+    # append the current loss term to costList to plot them later
+    costList.append(LPIPSLoss[0][0][0][0].item())
+
+    optimizer.step() # based on backpropagation implemented in lpips_loss.py
+
+
+# Save the output image
+if type(backgroundAndLabelImg) == torch.Tensor:
+        backgroundAndLabelImg = backgroundAndLabelImg.numpy()
+finalBackgroundAndLabel = Image.fromarray(backgroundAndLabelImg)
+finalBackgroundAndLabel.save("finalBackgroundAndLabel.jpg")
