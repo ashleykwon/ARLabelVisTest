7f8ad94e11b5400607bba8c6586537db34712f21
ashleykwon <hojung_kwon@brown.edu>
2023-10-10 13:11:39 -0400

    Label generation code modified

--- Assets/ConnectionTest/LabelColorGenerationTest.py
+++ Assets/ConnectionTest/LabelColorGenerationTest.py
@@ -8,10 +8,9 @@ import lpips
 import torchvision
 from torch.autograd import Variable
 from matplotlib import pyplot as plt # for debugging purposes
-from tqdm import tqdm
 
 loss_fn = lpips.LPIPS(net='vgg',version=0.1) #changed from alex to vgg based on this documentation: https://pypi.org/project/lpips/#b-backpropping-through-the-metric
-# loss_fn.cuda()
+loss_fn.cuda()
 
 # b_path = "./test2.jpg"
 # b_w_l_path = "./test2AndLabel.jpg"
@@ -53,19 +52,17 @@ optimizer = torch.optim.Adam([pred,], lr=0.01, betas=(0.9, 0.999))
 distanceThreshold = 0.23
 
 # Optimize
-for iter in tqdm(range(MAX_ITER)):
+for iter in range(MAX_ITER): 
     if type(backgroundAndLabelImg) == torch.Tensor:
         backgroundAndLabelImg = backgroundAndLabelImg.numpy()
 
     # Apply the boolean mask for label pixels
-    # maskedLabelTensor = torch.mul(pred, labelMaskAsTensor)
-    maskedLabelTensor = pred.where(labelMaskAsTensor, -1) # sets all non-label pixels to 1
-    maskedLabelTensor = blur_masked_label(maskedLabelTensor)
+    maskedLabelTensor = torch.mul(pred, labelMaskAsTensor)
+    # maskedLabelTensor = pred.where(torch.logical_not(labelMaskAsTensor), -1) # don't use this # sets all non-label pixels to -1
 
     # Calculate the LPIPS loss: -1 * LPIPS distance between the current backgroundAndLabelImage and the backgroundImage
     LPIPSLoss = loss_fn.forward(maskedLabelTensor.cuda(), backgroundImgAsTensor.cuda())
     neg_loss = - LPIPSLoss
-    neg_loss = - pixel_distance_loss(maskedLabelTensor)
     
     # Clear the gradient for a new calculation
     optimizer.zero_grad()
@@ -95,31 +92,3 @@ for iter in tqdm(range(MAX_ITER)):
         #  # Check the size of the image after backpropping
         #  width, height = Image.open(output_path).size
         #  print(f"Image width: {width}px, Image height: {height}px")
-
-
-def pixel_distance_loss(maskedLabel, weigth = 1.0):
-  image = maskedLabel.permute(2, 3, 1, 0).detach().numpy()
-  image = image.squeeze()
-  # plt.imshow(image)
-  image = np.where(image < 0, 0, image)
-
-  diff = np.sum(np.abs(np.diff(image, axis=0))) + np.sum(np.abs(np.diff(image, axis=1)))
-  diff_weight = weight * diff / image.size
-
-  x = torch.tensor([diff_weight])
-  return x.cuda()
-
-def blur_masked_label(maskedLabel):
-  image = maskedLabel.permute(2, 3, 1, 0).detach().numpy()
-  image = image.squeeze()
-  blurred_image = np.empty_like(image)
-
-  black_mask = np.all(image != [0, 0, 0], axis=-1)
-
-  for channel in range(3):
-    blurred_image[:, :, channel] = gaussian_filter(image[:, :, channel] * black_mask, sigma=1)
-  
-  # plt.imshow(blurred_image)
-  blurred_image = np.expand_dims(blurred_image, axis=-1)
-  blurred_image = np.transpose(blurred_image, (3, 2, 0, 1))
-  return torch.tensor(blurred_image)
