9408d396400981ea446854f3d3292abdd0438c7b
ashleykwon <hojung_kwon@brown.edu>
2024-02-14 11:01:25 -0500

    tried overlaying ssim and opaque

--- Assets/ConnectionTest/AreaColorGenerationTest.py
+++ Assets/ConnectionTest/AreaColorGenerationTest.py
@@ -132,7 +132,8 @@ if __name__ == '__main__':
                                                                      help='Paths to input images')
     parser.add_argument('--imageAndLabel_paths', nargs='+', default=[
                                                                     # './testImages/testCurry/curryAndLabel_white.jpg'
-                                                                    './testImages/testFruit/fruitAndBlueArea.jpg' 
+                                                                    './testResults/test20240201_noInitialColor/fruit_opcityResult.jpg' 
+                                                                    # './testImages/testFruit/fruitAndBlueArea.jpg'
                                                                     #  ,'./testRiver/riverAndLabel.jpg'
                                                                     #  ,'./testRiver/riverAndLabel_white.jpg'
                                                                     #  ,'./testSingleColor/blueAndLabel.jpg'
@@ -376,9 +377,9 @@ if __name__ == '__main__':
                 
                 if args.metric == 'ssim':
                     # output_path = f"./testResults/test20240201/{image_name}_weight-{weight}_{args.metric}_a-{alpha}b-{beta}c-{gamma}_unblurredBG_itr{args.itr}_lr{args.lr}_deltaE-{args.deltaE}.jpg"
-                    output_path = f"./testResults/test20240207_initialBlue/{image_name}_weight-{weight}_{args.metric}_s-{ssim_sigma}k1-{k1}k2-{k2}_itr{args.itr}_lr{args.lr}_deltaE-{args.deltaE}.jpg"
+                    output_path = f"./testResults/test20240214_initialOpaque/{image_name}_weight-{weight}_{args.metric}_s-{ssim_sigma}k1-{k1}k2-{k2}_itr{args.itr}_lr{args.lr}_deltaE-{args.deltaE}.jpg"
                 else:
-                    output_path = f"./testResults/test20240207_initialBlue/{image_name}_weight-{weight}_{args.metric}_itr{args.itr}_lr{args.lr}_deltaE-{args.deltaE}.jpg"
+                    output_path = f"./testResults/test20240214_initialOpaque/{image_name}_weight-{weight}_{args.metric}_itr{args.itr}_lr{args.lr}_deltaE-{args.deltaE}.jpg"
                 Image.fromarray(pred_img).save(output_path)
                 break
 
@@ -393,169 +394,3 @@ if __name__ == '__main__':
 
 
 
-
-
-"""
-######-------------------------Original code------------------------------------------------
-loss_fn = lpips.LPIPS(net='vgg',version=0.1) #changed from alex to vgg based on this documentation: https://pypi.org/project/lpips/#b-backpropping-through-the-metric
-loss_fn.cuda()
-
-# Test images downloaded from online sources
-# b_path = "./testImg2/test2.jpg"
-# b_w_l_path = "./testImg2/test2AndLabel.jpg"
-# labelMask_path = "./testImg2/test2AndLabel_mask.jpg"
-
-b_path = "./testCurry/curry.jpg"
-b_w_l_path = "./testCurry/curryAndLabel_white.jpg"
-labelMask_path = "./testCurry/curryMask.jpg"
-labelMaskImg = (1/255)*np.asarray(Image.open(labelMask_path))
-
-# # Screenshots from the AR headset
-# b_path = "./background.jpg"
-# # b_w_l_path = "./backgroundAndLabel.jpg"
-# b_w_l_path = "./test_backgroundAndLabel.jpg"
-# labelMask_path = "./test_mask.jpg"
-# labelMaskImg = (1/255)*np.asarray(Image.open(labelMask_path))
-
-# # Check the size of the image before backpropping
-# width, height = Image.open(b_w_l_path).size
-# print(f"Image width: {width}px, Image height: {height}px")
-
-
-
-# # --------------Load images and convert them to tensors-----------------------------------------------------
-# Convert the images to tensors
-backgroundImgAsTensor = lpips.im2tensor(lpips.load_image(b_path))
-backgroundAndLabelImgAsTensor = lpips.im2tensor(lpips.load_image(b_w_l_path))
-height = backgroundImgAsTensor.size(2)
-width = backgroundImgAsTensor.size(3)
-numPixels = height * width  # 512*1024 for AR screenshots
-# print("image size: ", backgroundImgAsTensor.size())
-
-# The predicted image
-# pred = Variable(backgroundAndLabelImgAsTensor, requires_grad=True)
-
-
-# # --------------Separate label and background and set different requires_grad values for them -> only gradient descent on label pixels-------------------------
-# create label mask (boolean mask with True where label pixels are)
-labelMaskAsTensor = lpips.im2tensor(lpips.load_image(labelMask_path)) > 0
-numLabelPixels = labelMaskAsTensor[0,0].sum().item() # number of label pixels
-
-# Flatten image and mask to apply the mask
-imageFlat = backgroundAndLabelImgAsTensor.view(1,3,-1) #[1,3,numPixels]
-maskFlat = labelMaskAsTensor.view(1,3,-1) #[1,3,numPixels]
-
-# Separate background and label pixels
-labelFlat = imageFlat[:, maskFlat[0]] #[1,numLabelPixels]
-# backgroundFlat = imageFlat[ :, ~maskFlat[0]] # not used
-
-# Set them to torch variables for back-propagation
-labelVar = Variable(labelFlat, requires_grad=True)
-# backgroundVar = Variable(imageFlat, requires_grad=False) # not used
-
-# Get indices of label pixels as a tuple of d tensors where d is the number of dimensions -> ([x1, x2, x3, ..., xn], [y1, y2, y3, ..., yn], ...)
-labelIndices = torch.nonzero(maskFlat, as_tuple=True) # Here, d = 3 and n = 3*numLabelPixels
-
-
-# # --------------Try blurring the background image -- Gaussian blur ---------------------------------------------------------
-sigma = 15
-# r = 60
-backgroundImgAsTensor = scipy.ndimage.gaussian_filter(backgroundImgAsTensor, sigma=(0, 0, sigma, sigma), radius=None)
-backgroundImgAsTensor = torch.from_numpy(backgroundImgAsTensor)
-
-imgReshaped = imageFlat.view(1,3,height,width)
-imgReshaped = scipy.ndimage.gaussian_filter(imgReshaped, sigma=(0, 0, sigma, sigma), radius=None)
-# print(type(imgReshaped))
-imgReshaped = torch.from_numpy(imgReshaped)
-imageBlurred = imgReshaped.view(1,3,-1)
-
-
-# # --------------Set optimizer and start iterating--------------------------------------------------------------------------
-# stochastic gradient descent-based optimizer
-# optimizer = torch.optim.SGD([torch.from_numpy(backgroundAndLabelImg)], lr=0.01, momentum=0.9)
-# optimizer = torch.optim.SGD([pred], lr=0.05, momentum=0.9)
-
-# Adam optimizer, original lr=1e-4
-optimizer = torch.optim.Adam([labelVar,], lr=0.08, betas=(0.9, 0.999))
-
-distanceThreshold = 0.23
-
-MAX_ITER = 1000
-costList = []
-
-# Optimize
-for iter in range(MAX_ITER): 
-    # if type(backgroundAndLabelImg) == torch.Tensor:
-    #     backgroundAndLabelImg = backgroundAndLabelImg.numpy()
-
-    # Apply the boolean mask for label pixels
-    # maskedLabelTensor = torch.mul(pred, labelMaskAsTensor)
-    # maskedLabelTensor = pred.where(torch.logical_not(labelMaskAsTensor), -1) # don't use this # sets all non-label pixels to -1
-
-    # initialize to the origianl full image (does not matter what the pixels in label region are bc they will be overwritten later)
-    full_img = imageBlurred  # torch.Size([1, 3, 524288])
-    # full_img = imageFlat # background not blurred
-
-    # Overwrite the label pixels using the updated results
-    full_img.index_put_(labelIndices, labelVar.reshape(labelVar.size()[1]))  # labelVar size: torch.Size([1, numLabelPixels]) -> reshape it to [numLabelPixels] to fit in index_put_()
-    full_img = full_img.view(1,3,height,width) # restore its shape to match the original image's shape
-    full_img.data = torch.clamp(full_img.data, -1, 1)
-
-    # Calculate the LPIPS loss: LPIPS distance between the current backgroundAndLabelImage and the backgroundImage
-    LPIPSLoss = loss_fn.forward(full_img.cuda(), backgroundImgAsTensor.cuda())
-
-    # Negate the loss to make the image more and more different from the original one
-    neg_loss = - LPIPSLoss
-    
-    # Clear the gradient for a new calculation
-    optimizer.zero_grad()
-
-    # Do backpropagation based on the LPIPS loss above
-    neg_loss.backward(retain_graph=True)
-
-    # append the current loss term to costList to plot them later
-    # costList.append(LPIPSLoss[0][0][0][0].item())
-
-    optimizer.step() # based on backpropagation implemented in lpips_loss.py
-
-    # Print out losses/distances
-    if iter % 100 == 0:
-        print('iter %d, dist %.3g' % (iter, LPIPSLoss.view(-1).data.cpu().numpy()[0]))        
-
-    # Save the output image
-    if (iter == MAX_ITER - 1): 
-        print('Reached the last iteration')
-        # Get the unblurred background + overlay with label
-        full_img = imageFlat # a tensor
-        full_img.index_put_(labelIndices, labelVar.reshape(labelVar.size()[1]))  # labelVar size: torch.Size([1, numLabelPixels]) -> reshape it to [numLabelPixels] to fit in index_put_()
-        full_img = full_img.view(1,3,height,width) # restore its shape to match the original image's shape -- this is unblurred label with unblurred background
-        full_img.data = torch.clamp(full_img.data, -1, 1)
-
-        # # ---------------------Try blurring the label-------------------------------------------
-        # full_img = cv2.imread(b_w_l_path)
-        # mask = (1/255)*cv2.imread(labelMask_path) 
-
-        # blurred_label = full_img
-        # blurred_label[mask > 0.5] = scipy.ndimage.gaussian_filter(full_img, sigma=(3, 3, 0))[mask > 0.5]
-        # combined = full_img
-        # combined[mask > 0.5] = blurred_label[mask > 0.5]
-        # # output_img =  lpips.tensor2im(combined.data)
-
-        # output_path = "./tests/test003.jpg"
-        # cv2.imwrite(output_path, combined)
-
-        
-        
-        pred_img = lpips.tensor2im(full_img.data)
-        print(type(pred_img))
-        output_path = "./testCurry/blurredBG_lr0.08_sigma15_itr1000_white.jpg"
-        # output_path = "./final_result_adam_lr0.08.jpg"
-        Image.fromarray(pred_img).save(output_path)
-        break
-
-        #  # Check the size of the image after backpropping
-        #  width, height = Image.open(output_path).size
-        #  print(f"Image width: {width}px, Image height: {height}px")
-
-
-"""
--- Assets/ConnectionTest/AreaColorOpacityOnly.py
+++ Assets/ConnectionTest/AreaColorOpacityOnly.py
@@ -25,6 +25,8 @@ from colormath.color_diff import delta_e_cie1976
 from matplotlib import pyplot as plt # for debugging purposes
 from kornia.color import rgb_to_lab
 
+'''
+###------------Generates images with opaque highlighted area------------------------------------------------------------
 background = cv2.imread('./testImages/testFruit/fruit.jpg')
 mask = cv2.imread('./testImages/testFruit/areaMask.jpg', cv2.IMREAD_GRAYSCALE)
 _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
@@ -61,3 +63,33 @@ overlay2 = overlay2.astype(np.float32) #/ 255.0
 result2 = cv2.addWeighted(background2, 1.0, overlay2, 1, 0)
 # cv2.imshow('Hightlighted', result)
 cv2.imwrite('./testResults/test20240201_noInitialColor/board_opcityResult_1.jpg', result2)
+'''
+
+###------------overlay two images------------------------------------------------------------
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+
+# background = Image.open("./testResults/test20240201/fruit_weight-0.05_ssim_s-0.1k1-0.01k2-0.03_itr100_lr0.08_deltaE-False.jpg")
+# overlay = Image.open("./testResults/test20240201_noInitialColor/fruit_opcityResult.jpg")
+background = Image.open('./testImages/testFruit/fruit.jpg')
+overlay = Image.open('./testResults/test20240214_initialOpaque/edges.png')
+
+background = background.convert("RGBA")
+overlay = overlay.convert("RGBA")
+
+ratio = 0.3
+new_img = Image.blend(background, overlay, ratio) # ratio*overlay + (1-ratio)*background
+new_img.save(f"./testResults/test20240214_initialOpaque/overlay_edges_{ratio}.png")
+
+# img = cv2.imread('./testImages/testFruit/fruit.jpg')
+# # Convert to graycsale
+# img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# # Blur the image for better edge detection
+# img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
+# # Canny Edge Detection
+# edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) # Canny Edge Detection
+# # Display Canny Edge Detection Image
+# cv2.imshow('Canny Edge Detection', edges)
+# cv2.imwrite('./testResults/test20240214_initialOpaque/edges.png', edges)
